ec2a8e0495dbaf1545ae4a269b9acc2b0d78d1db
javier32rojas040506 <59894906+javier32rojas040506@users.noreply.github.com>
2023-02-01 18:54:44 -0500

    first cm

---
+++ src/test/java/controller/health/ApplicationTests.java
@@ -0,0 +1,13 @@
+package controller.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
